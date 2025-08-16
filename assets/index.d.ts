4d6bedf6ea22230b839e94e138e55b069ea76d2c
nicolaslps <lopesn.pro@gmail.com>
2025-08-16 19:13:48 +0200

    link primitive

---
+++ assets/index.d.ts
@@ -0,0 +1,2 @@
+// Re-export all types from primitives
+export * from '@harmonyui/primitives';
---
+++ assets/index.js
@@ -0,0 +1,5 @@
+// Re-export all primitives components for easy consumption
+export * from '@harmonyui/primitives';
+
+// You can also import specific components if needed:
+// export { Accordion } from '@harmonyui/primitives/accordion';
