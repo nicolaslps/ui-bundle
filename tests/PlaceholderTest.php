71265f7840f79b982e36e02589b5464246130842
nicolaslps <lopesn.pro@gmail.com>
2025-08-18 22:42:37 +0200

    placeholder test

---
+++ tests/PlaceholderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the HarmonyUI project.
+ *
+ * (c) Nicolas Lopes
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarmonyUi\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
